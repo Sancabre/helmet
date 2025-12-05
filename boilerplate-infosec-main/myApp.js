280e136924a44353a80732d18e3d8f4490d4d1c6
Sancabre <167893745+Sancabre@users.noreply.github.com>
2025-12-05 23:03:11 +0000

    update

--- boilerplate-infosec-main/myApp.js
+++ boilerplate-infosec-main/myApp.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 const app = express();
 
@@ -8,15 +7,4 @@ app.use(helmet());
 // 👈 HASTA AQUÍ
 
 module.exports = app;
-const api = require('./server.js');
-app.use(express.static('public'));
-app.disable('strict-transport-security');
-app.use('/_api', api);
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
-});
-let port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Your app is listening on port ${port}`);
-});
-hello;
+
